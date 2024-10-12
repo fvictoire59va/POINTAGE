d2a5e935d3d90669c104f73211ac0e951227a7d5
fvictoire <frederic.victoire@gmail.com>
2024-10-12 11:43:47 +0200

    restitution jour, semaine, mois N°1

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def lire_xlsx(nom_du_fichier):
     return df
 
 # Utility function
-def convert_html_to_pdf(template, df, output_filename):
-    source_html = template.render(df=df)
+def convert_html_to_pdf(template, df, output_filename, titre):
+    source_html = template.render(df=df, titre=titre)
     result_file = open(output_filename, "w+b")
     pisa_status = pisa.CreatePDF(
             source_html,                # the HTML to convert
@@ -34,36 +34,53 @@ def convert_html_to_pdf(template, df, output_filename):
 
 if __name__ == "__main__":
     df_pointage = lire_xlsx('data/pointage.xlsx')
-    # aggrégé a la journée et par employé
-    aggregated_df_jour = df_pointage.groupby(['id_employe', 'weeknumber','date']).agg({'nb_heure': 'sum', 'conducteur': 'sum', 'passager': 'sum'})
-    aggregated_df_jour['repas'] = np.where(aggregated_df_jour['nb_heure'] >= 5, 1, 0)
-
     df_employe = lire_xlsx('data/employe.xlsx')
 
-    # jointure sur la table de depart sur la semaine et l'employe
-    merged_df = pd.merge(df_pointage, df_employe, on=['id_employe'], how='left')
-
+    aggregated_df_jour = df_pointage.groupby(['id_employe', 'mois', 'weeknumber', 'date']).agg({'nb_heure': 'sum', 'conducteur': 'max', 'passager': 'max'})
+    aggregated_df_jour['repas'] = np.where(aggregated_df_jour['nb_heure'] >= 5, 1, 0)
+    aggregated_df_jour = aggregated_df_jour.reset_index()
+    merged_df = pd.merge(aggregated_df_jour, df_employe, on=['id_employe' ], how='left')
     merged_df = merged_df.rename(columns={'nb_heure': 'heures',
-                    'id_employe': 'employe',
-                    'id_chantier': 'chantier',
-                    'conducteur': 'chauffeur',
-                    'weeknumber': 'semaine',
-                    'passager': 'passager'
-                    })
-
-    # Configuration de Jinja2 pour charger le template
+                       'conducteur': 'chauffeur',
+                       'weeknumber': 'semaine',
+                       'passager': 'passager'
+                     })
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('template/pointage_jour.html')
-
-    dfs_par_id = dict(list(merged_df.groupby('employe')))
-
+    dfs_par_id = dict(list(merged_df.groupby('id_employe')))
     pisa.showLogging()
- 
+    df_check = pd.DataFrame()
+    df_check_global = pd.DataFrame()
     for id, df in dfs_par_id.items():
-        df.drop('employe', axis=1, inplace=True)
+        df.drop('id_employe', axis=1, inplace=True)
         df['date'] = df['date'].dt.strftime('%d-%m-%Y')
-        df = df[['prenom', 'nom', 'mois', 'semaine', 'date', 'chantier', 'heures', 'chauffeur', 'passager', 'zone']]
+        df = df[['prenom', 'nom', 'mois', 'semaine', 'date', 'heures', 'chauffeur', 'passager', 'repas']]
         df = df.sort_values(by='date')
         df = df.fillna(0)
-        df['Check'] = np.where(df['chauffeur'] + df['passager'] > 1 , 'ERREUR chauffeur ou passager', 'OK')
-        convert_html_to_pdf(template, df, str(df['prenom'].unique()[0]) + "_" + str(df['nom'].unique()[0]) + "_" + str(df['mois'].unique()[0]) + "_decompte_heures.pdf")
+        df_check = df[['date', 'prenom','nom']].copy()
+        # df_check['check chauffeur passager'] = np.where(df['chauffeur'] + df['passager'] > 1 , 'ERREUR chauffeur ou passager', 'OK')
+        # df_check['Check zone>7'] = np.where(df['zone'] > 7 , 'la zone ne peut pas etre > 7', 'OK')
+        # df_check['Check zone<1'] = np.where(df['zone'] < 1 , 'la zone ne peut pas etre < 1', 'OK')
+        # df_check=df_check[(df_check['check chauffeur passager'] != 'OK') | 
+        #             (df_check['Check zone>7'] != 'OK') |  
+        #             (df_check['Check zone<1'] != 'OK')
+        #            ]
+        # df_check_global=pd.concat([df_check_global, df_check], ignore_index=True)
+        convert_html_to_pdf(template, df, str(df['prenom'].unique()[0]) + "_" + str(df['nom'].unique()[0]) + "_" + str(df['mois'].unique()[0]) + "_decompte_heures_jours.pdf", "jour")
+    # convert_html_to_pdf(template, df_check_global, "check.pdf")
+
+    # aggrégé a la semaine pour calculer les heures supplémentaires, par employé
+    merged_df['id'] = merged_df['prenom'] + "_" + merged_df['nom']
+    merged_df_semaine = merged_df.groupby(['id', 'mois', 'semaine']).agg({'prenom': 'min', 'nom': 'min' , 'heures': 'sum', 'chauffeur': 'sum', 'passager': 'sum', 'repas': 'sum'})
+    merged_df_semaine['heures supp'] = np.where(merged_df_semaine['heures'] > 35 , merged_df_semaine['heures'] - 35, 0)
+    dfs_par_id = dict(list(merged_df_semaine.groupby('id')))
+    for id, df in dfs_par_id.items():
+        print(df)
+        df = df[['prenom', 'nom', 'mois', 'semaine', 'heures', 'chauffeur', 'passager', 'repas']]
+        convert_html_to_pdf(template, df, "pointage_semaine_" + str(df['prenom'].unique()[0]) + "_" + str(df['nom'].unique()[0]) + ".pdf", "semaine")
+
+    
+    # aggrégé au mois pour obtenir la fiche de salaire, par employé
+    # merged_df_mois = merged_df_semaine.groupby(['prenom', 'nom', 'mois']).agg({'heures': 'sum', 'chauffeur': 'sum', 'passager': 'sum', 'repas': 'sum', 'heures supp': 'sum'})
+    # convert_html_to_pdf(template, merged_df_mois, "pointage_mois.pdf")   
+    # print(merged_df_mois)
