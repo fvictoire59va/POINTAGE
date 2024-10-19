bfb8cc2064b2eb9a0fe5f47dd095e5105df3b465
fvictoire <frederic.victoire@gmail.com>
2024-10-19 10:52:00 +0200

    enchainement des methodes des dataframes

--- main.py
+++ main.py
@@ -33,8 +33,13 @@ def lire_xlsx(nom_du_fichier):
     return df
 
 # Utility function
-def convert_html_to_pdf(template, df, output_filename, unite_temps):
-    source_html = template.render(df=df, unite_temps=unite_temps)
+def convert_html_to_pdf(template, df, output_filename, unite_temps, df_zone=None):
+    df = df.round(2)
+    if df_zone is not None:
+        source_html = template.render(df=df, df_zone=df_zone, unite_temps=unite_temps)
+    else:
+        source_html = template.render(df=df, df_zone=None, unite_temps=unite_temps)
+    
     result_file = open(output_filename, "w+b")
     pisa_status = pisa.CreatePDF(
             source_html,                # the HTML to convert
@@ -44,19 +49,22 @@ def convert_html_to_pdf(template, df, output_filename, unite_temps):
     return pisa_status.err
 
 if __name__ == "__main__":
-    df_pointage = lire_xlsx('data/pointage.xlsx')
+    df_pointage = lire_xlsx('data/POINTAGE_JUIN_2024.xlsx')
     df_employe = lire_xlsx('data/employe.xlsx')
 
     # aggregation des lignes par employé, mois, semaine, jour
+    columns = {'nb_heure': 'heures',
+               'conducteur': 'chauffeur',
+               'weeknumber': 'semaine',
+               'passager': 'passager'
+              }
     aggregated_df_jour = df_pointage.groupby(['id_employe', 'mois', 'weeknumber', 'date']).agg({'nb_heure': 'sum', 'conducteur': 'max', 'passager': 'max'})
     aggregated_df_jour['repas'] = np.where(aggregated_df_jour['nb_heure'] >= 5, 1, 0)
     aggregated_df_jour = aggregated_df_jour.reset_index()
-    merged_df = pd.merge(aggregated_df_jour, df_employe, on=['id_employe' ], how='left')
-    merged_df = merged_df.rename(columns={'nb_heure': 'heures',
-                       'conducteur': 'chauffeur',
-                       'weeknumber': 'semaine',
-                       'passager': 'passager'
-                     })
+    merged_df = (aggregated_df_jour
+                 .merge(df_employe, on='id_employe', how='left')
+                 .rename(columns=columns)
+                )
     
     # calcul des zones a la journée
     # prendre la zone la plus pres quand il y a plusieurs chantiers sur la même journée
@@ -75,7 +83,7 @@ if __name__ == "__main__":
 
     # l'information concernant la zone est rattachée a la journée de l'employé
     merged_df = pd.merge(merged_df, df_zone, on=['date', 'id_employe' ], how='left')
-    df = df.fillna(0).infer_objects(copy=False)
+    merged_df = merged_df.fillna(0).infer_objects(copy=False)
     
     # recuperation du template pour la creation du tableau
     env = Environment(loader=FileSystemLoader('.'))
@@ -104,6 +112,15 @@ if __name__ == "__main__":
         df = df[['prenom', 'nom', 'mois', 'semaine', 'heures', 'chauffeur', 'passager', 'repas', 'heures supp']]
         convert_html_to_pdf(template, df, "result/pointage_semaine_" + str(df['prenom'].unique()[0]) + "_" + str(df['nom'].unique()[0]) + ".pdf", "semaine")
     
+    df_mois_zone = merged_df.groupby(['id_employe', 'mois']).agg({'zone': list}).reset_index()
+    dfs_zone_par_id = dict(list(df_mois_zone.groupby('id_employe')))
+    for id, df in dfs_zone_par_id.items():
+        df = df[['mois', 'zone', 'id_employe']]
+        df_zone_dec = pd.DataFrame(columns=['zone 1', 'zone 2', 'zone 3', 'zone 4', 'zone 5', 'zone 6', 'zone 7'])
+        list_zone = df.iloc[0, 1]
+        df_zone_dec.loc[len(df_zone_dec)] = [list_zone.count(1), list_zone.count(2), list_zone.count(3), list_zone.count(4), list_zone.count(5), list_zone.count(6), list_zone.count(7)]
+        df_transposed = df_zone_dec.T
+    
     # aggrégé au mois pour obtenir la fiche de salaire, par employé
     merged_df_mois = merged_df_semaine.groupby(['id', 'mois']).agg({'prenom': 'min', 'nom': 'min' , 'heures': 'sum', 'chauffeur': 'sum', 'passager': 'sum', 'repas': 'sum', 'heures supp': 'sum'})
     df = merged_df_mois.reset_index()   
@@ -111,22 +128,8 @@ if __name__ == "__main__":
     # pour chaque employé, genere un pdf constitué d'un tableau mensuel
     for id, df in dfs_par_id.items():
         df = df[['prenom', 'nom', 'mois', 'heures', 'chauffeur', 'passager', 'repas', 'heures supp']]
-        convert_html_to_pdf(template, df, "result/pointage_mois_" + str(df['prenom'].unique()[0]) + "_" + str(df['nom'].unique()[0]) + ".pdf", "mois") 
+        convert_html_to_pdf(template, df, "result/pointage_mois_" + str(df['prenom'].unique()[0]) + "_" + str(df['nom'].unique()[0]) + ".pdf", "mois", df_transposed) 
     
-    df_mois_zone = merged_df.groupby(['id_employe', 'mois']).agg({'zone': list}).reset_index()
-    dfs_zone_par_id = dict(list(df_mois_zone.groupby('id_employe')))
-    # df_zone_dec = pd.DataFrame(columns=['nb_zone1', 'nb_zone2', 'nb_zone3', 'nb_zone4', 'nb_zone5', 'nb_zone6', 'nb_zone7'])
-    for id, df in dfs_zone_par_id.items():
-        df = df[['mois', 'zone', 'id_employe']]
-        df_zone_dec = pd.DataFrame(columns=['zone 1', 'zone 2', 'zone 3', 'zone 4', 'zone 5', 'zone 6', 'zone 7'])
-        list = df.iloc[0, 1]
-        df_zone_dec.loc[len(df_zone_dec)] = [list.count(1), list.count(2), list.count(3), list.count(4), list.count(5), list.count(6), list.count(7)]
-        df_transposed = df_zone_dec.T
-        template = env.get_template('template/pointage_zone.html')
-        convert_html_to_pdf(template, df_transposed, "result/zones_mois_" + str(df['id_employe'].unique()[0]) + ".pdf", "mois") 
-    
-    # result = df.groupby(['date', 'id_employe'])['Values'].apply(lambda x: ', '.join(x)).reset_index()
-    # print(df)
     # controle des incohérences
     # controle 1 
     # si la somme des heures > 5 alors il n'y a pas de repas
